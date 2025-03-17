9cb475f49b7b039104fc3b5c0a1ff1987fe87cb1
adilzhanshukenov <shukenovadilzhan@gmail.com>
2025-03-17 19:13:59 +0500

    amre song

--- uzutu-invite/src/components/CircularMusicButton.tsx
+++ uzutu-invite/src/components/CircularMusicButton.tsx
@@ -1,7 +1,7 @@
 export default function CircularMusicButton() {
   return (
     <audio autoPlay loop>
-      <source src="/your-music-file.mp3" type="audio/mpeg" />
+      <source src="/audio/amre.mp3" type="audio/mpeg" />
       Your browser does not support the audio element.
     </audio>
   );
